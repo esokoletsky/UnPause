e1cf7ef2f964341f931fc2e0a2582c42540d17d7
unknown <esokoletsky@gmail.com>
2019-01-25 10:58:42 -0500

    updates to server.js with passport

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const passport = require('passport');
 // setup your config file to connect to your database
 const app = express();
 
+const { DATABASE_URL } = require('./config/database');
+
 app.set('port', (process.env.PORT || 3001));
 
 // Express only serves static assets in production
@@ -14,7 +16,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
+require('./config/passport')(passport);
 
+// required for passport
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
     // =====================================
     // LOGIN ===============================
@@ -34,7 +40,6 @@ if (process.env.NODE_ENV === 'production') {
       failureRedirect : '/login', // redirect back to the signup page if there is an error
       failureFlash : true // allow flash messages
       })
-      console.log('checking something')
   );
 
   // =====================================
@@ -57,9 +62,44 @@ if (process.env.NODE_ENV === 'production') {
   
 
 
+  let server; 
 
-
-
-app.listen(app.get('port'), () => {
-  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
-});
+  function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+    return new Promise((resolve, reject) => {
+      mongoose.connect(databaseUrl, err => {
+        if (err) {
+          return reject(err);
+        }
+        server = app.listen(port, () => {
+          console.log(`Your app is listening on port ${port}`);
+          resolve();
+        })
+          .on('error', err => {
+            mongoose.disconnect();
+            reject(err);
+          });
+      });
+    });
+  }
+  
+  
+  function closeServer() {
+    mongoose.disconnect();
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve();
+      });
+    });
+  }
+  
+  
+  if (require.main === module) {
+    runServer(DATABASE_URL).catch(err => console.error(err));
+  }
+  
+  module.exports = {app, runServer, closeServer};
